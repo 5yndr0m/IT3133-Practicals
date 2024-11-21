b64c7e4d5a6aa5a618313ae4eb11672c509b7ca5
5yndr0m <sdilanjana21@gmail.com>
2024-11-21 15:28:47 +0530

    feat: added practicals for Nov 05

---
+++ appenv/src/components/Nov_05/App0511.js
@@ -0,0 +1,12 @@
+import StudentInfo from './componenets/Nov_05/StudentInfo';
+
+function App() {
+  return (
+    <div className="App">
+      {/*<StudentInfo/>*/}
+      <StudentInfo/>
+    </div>
+  );
+}
+
+export default App;
