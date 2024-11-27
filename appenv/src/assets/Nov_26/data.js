3934fcacadacce625e664c25cc98a5dd5c0c38f9
5yndr0m <sdilanjana21@gmail.com>
2024-11-27 17:42:07 +0530

    feat: for storing user credentials

---
+++ appenv/src/assets/Nov_26/data.js
@@ -0,0 +1,7 @@
+const users = [
+  { email: "test@example.com", password: "12345" },
+  { email: "user1@example.com", password: "password1" },
+  { email: "admin@example.com", password: "admin123" },
+];
+
+export default users;
