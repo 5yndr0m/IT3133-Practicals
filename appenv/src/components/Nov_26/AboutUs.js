00214a1df898665e84f0ae559932641d7ec04afb
5yndr0m <sdilanjana21@gmail.com>
2024-11-27 16:07:25 +0530

    feat: added some context to the about us page

--- appenv/src/components/Nov_26/AboutUs.js
+++ appenv/src/components/Nov_26/AboutUs.js
@@ -1,9 +1,40 @@
-import '../../App.css'
+import '../../assets/Nov_26/styless.css';
 
 export default function AboutUs() {
   return (
-    <div lassName="App">
-      <h1>About Us</h1>
-    </div>
+      <div className="aboutContainer">
+        <h1 className="title">About Us</h1>
+        <hr />
+        <section>
+          <h3>Who are we?</h3>
+          <p>
+            We are a team of six, all of whom have a background in the fields of Computer Science, Mathematics, Physics, 
+            and Economics. Together, we are passionate about solving some of the world's toughest challenges through 
+            technology. We are always looking for ways to push the boundaries of what's possible, no matter how big or small. 
+            Our team members are not only skilled in the tech stack we utilize, but also in the areas we serve. Whether you're a 
+            seasoned professional or just starting out, we are committed to helping you achieve your goals and reach for the stars.
+          </p>
+        </section>
+        <hr />
+        <section>
+          <h3>What can you expect?</h3>
+          <p>
+            We offer a wide range of services to meet your needs, from coding and web development to project management and 
+            team building. We pride ourselves on delivering high-quality solutions that exceed our clients' expectations. Whether 
+            you're looking for a custom software solution, a one-on-one consultation, or a team of experts to help you grow, 
+            we are here to support you every step of the way.
+          </p>
+        </section>
+        <hr />
+        <section>
+          <h3>What sets us apart?</h3>
+          <p>
+            Our diverse range of backgrounds and experiences allows us to approach problems from unique angles and provide 
+            tailored solutions. We are committed to continuous learning and improvement, and we believe that the more we grow 
+            as a team, the better we will become at what we do. That's why we foster a challenging and supportive work 
+            environment where we can push ourselves to be the best version of ourselves.
+          </p>
+        </section>
+      </div>
   );
 }
