e1677be40c8cbaf59656322ec8471bd2d56ca8bc
5yndr0m <sdilanjana21@gmail.com>
2024-11-27 16:41:19 +0530

    style: added some styles to the nav bar

--- appenv/src/components/Nov_26/App.js
+++ appenv/src/components/Nov_26/App.js
@@ -1,35 +1,49 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
 import Home from "./components/Nov_26/Home";
 import AboutUs from "./components/Nov_26/AboutUs";
 import ContactUs from "./components/Nov_26/ContactUs";
 import Login from "./components/Nov_26/Login";
-import './App.css'
+import './App.css';
+import './assets/Nov_26/styless.css';
 
 function App() {
- 
   return (
     <div className="App">
-     <Router>
-       <nav>
-         <ul>
-           <li>
-             <NavLink to="/">Home</NavLink>
-           </li>
-           <li>
-             <NavLink to="/about">About</NavLink>
-           </li>
-           <li>
-             <NavLink to="/contact">Contact</NavLink>
-           </li>
-         </ul>
-       </nav>
-       <Routes>
-         <Route path="/" element={<Home />} />
-         <Route path="/about" element={<AboutUs />} />
-         <Route path="/contact" element={<ContactUs />} />
-       </Routes>
-     </Router>
+      <Router>
+        <nav className="navStyles">
+          <ul className="navUl">
+            <li>
+              <NavLink to="/" className={({ isActive }) => `inactive-link ${isActive ? 'active-link' : ''}`}>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" className={({ isActive }) => `inactive-link ${isActive ? 'active-link' : ''}`}>
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact" className={({ isActive }) => `inactive-link ${isActive ? 'active-link' : ''}`}>
+                Contact
+              </NavLink>
+            </li>
+            <li style={{ marginLeft: "auto" }}>
+              <NavLink to="/login" className={({ isActive }) => `button-link ${isActive ? 'active-button' : ''}`}>
+                Logout
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<div>Page Not Found</div>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
