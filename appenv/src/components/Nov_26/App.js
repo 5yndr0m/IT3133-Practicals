b64be851e2a6a947f25cd94818e8b6e3f7922616
5yndr0m <sdilanjana21@gmail.com>
2024-11-27 17:01:25 +0530

    feat: wrapped nav link
    nav links are wrapped in a different function

--- appenv/src/components/Nov_26/App.js
+++ appenv/src/components/Nov_26/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, NavLink, useLocation } from "react-router-dom";
 import Home from "./components/Nov_26/Home";
 import AboutUs from "./components/Nov_26/AboutUs";
 import ContactUs from "./components/Nov_26/ContactUs";
@@ -9,22 +9,48 @@ import './assets/Nov_26/styless.css';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
+    <Router>  
+      <Navigation />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="*" element={<div>Page Not Found</div>} />
+      </Routes>
+    </Router>
+  );
+}
+
+function Navigation() {
+  const location = useLocation();
+
+  return (
+    <div>
+      {location.pathname !== "/login" && (
         <nav className="navStyles">
           <ul className="navUl">
             <li>
-              <NavLink to="/" className={({ isActive }) => `inactive-link ${isActive ? 'active-link' : ''}`}>
+              <NavLink
+                to="/home"
+                className={({ isActive }) => `inactive-link ${isActive ? 'active-link' : ''}`}
+              >
                 Home
               </NavLink>
             </li>
             <li>
-              <NavLink to="/about" className={({ isActive }) => `inactive-link ${isActive ? 'active-link' : ''}`}>
+              <NavLink
+                to="/about"
+                className={({ isActive }) => `inactive-link ${isActive ? 'active-link' : ''}`}
+              >
                 About
               </NavLink>
             </li>
             <li>
-              <NavLink to="/contact" className={({ isActive }) => `inactive-link ${isActive ? 'active-link' : ''}`}>
+              <NavLink
+                to="/contact"
+                className={({ isActive }) => `inactive-link ${isActive ? 'active-link' : ''}`}
+              >
                 Contact
               </NavLink>
             </li>
@@ -35,15 +61,7 @@ function App() {
             </li>
           </ul>
         </nav>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<div>Page Not Found</div>} />
-        </Routes>
-      </Router>
+      )}
     </div>
   );
 }
